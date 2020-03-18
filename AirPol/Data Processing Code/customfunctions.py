295dc035d74f10bf84f14a8b80cfc7b5f86e966e
Astralknight532 <hanankwok532@gmail.com>
2020-03-18 14:13:07 -0400

    Further streamlining of the functions in customfunctions.py

--- AirPol/Data Processing Code/customfunctions.py
+++ AirPol/Data Processing Code/customfunctions.py
@@ -6,7 +6,7 @@ Created on Tue Mar 17 10:39:32 2020
 """
 
 import pandas as pd
-import math as m
+from math import sin, cos, sqrt, atan2, radians, pi
 
 # Defining functions to organize repetitive tasks
 
@@ -22,15 +22,15 @@ def float_convert(data:pd.Series) -> pd.Series:
 
 # Converting between degrees and radians (used in the function lat_long_dist)
 def d_to_r(deg): # degrees to radians
-    return (deg * m.pi) / 180
+    return (deg * pi) / 180
 def r_to_d(rad): # radians to degrees
-    return (rad * 180) / m.pi
+    return (rad * 180) / pi
 
 # Distance between 2 latitude-longitude points in kilometers
 def lat_long_dist(lat1, long1, lat2, long2):
     earth_r = 6378 # radius of the Earth in kilometers
-    lat_dif = d_to_r(lat2 - lat1)
-    long_dif = d_to_r(long2 - long1)
-    s1 = (m.sin(lat_dif / 2) ** 2) + (m.cos(d_to_r(lat1)) * m.cos(d_to_r(lat2))) * (m.sin(long_dif / 2) ** 2)
-    s2 = 2 * m.atan2(m.sqrt(s1), m.sqrt(1 - s1))
+    lat_dif = radians(lat2) - radians(lat1)
+    long_dif = radians(long2) - radians(long1)
+    s1 = (sin(lat_dif / 2) ** 2) + (cos(radians(lat1)) * cos(radians(lat2))) * (sin(long_dif / 2) ** 2)
+    s2 = 2 * atan2(sqrt(s1), sqrt(1 - s1))
     return earth_r * s2
