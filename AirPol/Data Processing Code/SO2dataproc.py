4cbf286cc582e096468a0891a04deab900bf357a
Astralknight532 <hanankwok532@gmail.com>
2020-03-20 09:44:13 -0400

    Updated the code for processing the remaining data

--- AirPol/Data Processing Code/SO2dataproc.py
+++ AirPol/Data Processing Code/SO2dataproc.py
@@ -5,3 +5,15 @@ Created on Tue Mar 17 10:50:44 2020
 @author: hanan
 """
 
+# Import needed libraries
+#import customfunctions as cf # a Python file with functions I wrote for repetitive tasks
+import os
+import pandas as pd
+from numpy import array
+from keras.preprocessing.sequence import TimeseriesGenerator
+from keras.optimizers import SGD
+from keras.models import Sequential
+from keras.layers import Dense, LSTM, Dropout
+import matplotlib.pyplot as plt
+#import plotly.graph_objects as go
+#import plotly.express as px
